0b4c2f73465a4b039a013db11b7543343e56d1d2
Ryan Delaney <vitineth@gmail.com>
2019-02-28 18:53:24 +0000

    Setting up the main class which runs as the program should
    Currently will produce an empty output because we do not have a valid slide creator method
    
    Took 8 minutes

--- src/coffee/hashcode/PhotoSlideshow.java
+++ src/coffee/hashcode/PhotoSlideshow.java
@@ -3,11 +3,20 @@ package coffee.hashcode;
 import coffee.hashcode.parser.ImageParser;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class PhotoSlideshow {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(ImageParser.parseInputFile(FileUtilities.readFileFully(new File("datasets/a_example.txt"))));
+        File inputFile = new File("datasets/a_example.txt");
+        String fileData = FileUtilities.readFileFully(inputFile);
+        HashMap<String, List<Image>> categorisedImages = ImageParser.parseInputFile(fileData);
+        SlideCreator creator = new SlideCreator(categorisedImages);
+        List<Slide> slideShow = creator.getSlideShow();
+
+        OutputWriter outputWriter = new OutputWriter();
+        outputWriter.print(slideShow);
     }
 
 }

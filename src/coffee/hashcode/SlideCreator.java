2894130f79c31f621736c5bbe864f08c26b88bb3
Ryan Delaney <vitineth@gmail.com>
2019-02-28 19:17:44 +0000

    Adding basic pairing implementation
    Took 12 minutes

--- src/coffee/hashcode/SlideCreator.java
+++ src/coffee/hashcode/SlideCreator.java
@@ -1,8 +1,12 @@
 package coffee.hashcode;
 
+import coffee.hashcode.Image.Orientation;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SlideCreator
 {
@@ -23,9 +27,37 @@ public class SlideCreator
     private HashMap<String, List<Slide>> preprocessVerticals(){
         HashMap<String, List<Slide>> output = new HashMap<>();
 
+        List<Image> images = slides.values().stream().flatMap(Collection::stream).sorted(Comparator.comparing(Image::getTagNumber)).collect(Collectors.toList());
+        List<Image> vImages = images.stream().filter(i -> i.getOrientation() == Orientation.VERTICAL).collect(Collectors.toList());
+        List<Image> hImages = images.stream().filter(i -> i.getOrientation() == Orientation.HORIZONTAL).collect(Collectors.toList());
+
+        for (int i = 0; i < Math.floor(vImages.size()); i++) {
+            insertAtAllTagLocations(output, new VSlide(vImages.get(i), vImages.get(vImages.size() - 1 - i)));
+        }
+        hImages.stream().map(HSlide::new).forEach(i -> insertAtAllTagLocations(output, i));
+
         // TODO: replace this with some magic
 
         return output;
+    }
+
+    /**
+     * Inserts the given image into all lists of images associated with the keys that the image has. For any entry that does not already exist it
+     * will create a new array list in the output hashmap
+     *
+     * @param output the hashmap to insert the image into
+     * @param image  the image to insert into the hashmap
+     */
+    private static void insertAtAllTagLocations(HashMap<String, List<Slide>> output, Slide image) {
+        for (String tag : image.getTags()) {
+            if (output.containsKey(tag)) {
+                output.get(tag).add(image);
+            } else {
+                List<Slide> collection = new ArrayList<>();
+                collection.add(image);
+                output.put(tag, collection);
+            }
+        }
     }
 
     /**

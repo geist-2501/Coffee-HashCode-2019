7a49321deaac590b998f26ed26f3d68fbbf58126
Ryan Delaney <vitineth@gmail.com>
2019-02-28 18:15:42 +0000

    Adding image object
    Objects should mirror the format described in the spec
    
    Took 12 minutes

---
+++ src/coffee/hashcode/Image.java
@@ -0,0 +1,54 @@
+package coffee.hashcode;
+
+import java.util.Set;
+
+/**
+ * Represents one data record in the input file which itself represents an image
+ */
+public class Image {
+
+    public enum Orientation{
+        HORIZONTAL, VERTICAL
+    }
+
+    /**
+     * The ID of the image in the input file (line number - 1)
+     */
+    private int id;
+    /**
+     * If the photo is vertical (V) or horizontal (H)
+     */
+    private Orientation orientation;
+    /**
+     * The number of tags as specified in the file
+     */
+    private int tagNumber;
+    /**
+     * All tags in the file
+     */
+    private Set<String> tags;
+
+    public Image(int id, Orientation orientation, int tagNumber, Set<String> tags) {
+        this.id = id;
+        this.orientation = orientation;
+        this.tagNumber = tagNumber;
+        this.tags = tags;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public int getTagNumber() {
+        return tagNumber;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+}
